03ff199de489dc0cbfddebf493752fe576bd4bce
shubhamsingh41 <shubham110904@gmail.com>
2025-02-05 14:59:46 +0530

    4 commit

--- string.java
+++ string.java
@@ -1,7 +1,25 @@
 public class string {
     public static void main(String[] args) {
         String name = "shubham";
-        System.err.println(name);
+        //concatenation
+       String lname =" kumar singh";
+        String fname = name +""+ lname;
+        System.err.println(fname);
+        //length
+        System.err.println(fname.length());
+        //access character
+        for(int i = 0;i<fname.length();i++){
+            System.out.println(fname.charAt(i));
+            
+        
+        }
+        //substring
+        System.out.println(fname.substring(0,8));
+        //prase
+        String str ="123";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+    }
     }
     
-}
+
