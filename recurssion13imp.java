d9ebc7332ce3d701690867cdc2b005d6eecb0324
shubhamsingh41 <shubham110904@gmail.com>
2025-02-07 19:57:01 +0530

    14 commit

---
+++ recurssion13imp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//print all seguence of string 
+//bahut imp topic hai
+
+
+public class recurssion13imp {
+    public static void seq(String s , int i ,String news) {
+        
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+    }
+}
