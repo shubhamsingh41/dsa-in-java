321ae1fa5086d4a72e8b6af940e1201875edbff9
shubhamsingh41 <shubham110904@gmail.com>
2025-02-22 21:19:15 +0530

    41 commit

--- streak.java
+++ streak.java
@@ -1,5 +1,5 @@
 public class streak{
     public static void main(String[] args) {
-        System.out.println("only for maintaing streak");
+        System.out.println("only more day for maintaing streak");
     }
 }
