a69f094a601733810727e3f98118ba5361ca0cd9
shubhamsingh41 <shubham110904@gmail.com>
2025-02-27 21:12:34 +0530

    46 commit

---
+++ ll.java
@@ -0,0 +1,38 @@
+
+import java.util.LinkedList;
+
+
+
+
+public class ll {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+
+        list.addFirst("this");//yaha pai sirf add likhne sse nahi chalta first mai add karane ke liye likhna padta hai pura
+
+        list.addLast("list");//agr mai yaha sirf add likhta tabhi last mai hi add hota by efault set rehta hai
+
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " -> ");
+            
+        }
+        System.out.println("null");
+
+       /*  list.removeFirst();
+        System.out.println(list);
+
+        list.removeLast();
+        System.out.println(list);*/
+        list.remove(3);
+        System.out.println(list);
+    }
+}
+//ye jada aacha hai linllist ke liye
