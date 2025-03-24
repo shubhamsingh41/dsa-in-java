f02a0b9d51814d9deeec7fc4ac4ffc20f2f45249
shubhamsingh41 <shubham110904@gmail.com>
2025-03-24 15:35:56 +0530

    49 commit

---
+++ stackclass.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+//arraylist se stack kiye hai
+public class stackclass {
+    static class stack{
+       static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean isempty(){
+            return list.size() == 0;
+
+        }
+        public static void push(int data) {
+            list.add(data);
+            
+        }
+        //pop
+        public static int pop() {
+            if(isempty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+            
+        }
+        //peek
+        public static int peek() {
+            if(isempty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+          
+        while(!s.isempty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+        
+    }
+}
