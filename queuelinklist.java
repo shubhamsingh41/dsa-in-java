2b8bf474eedc822a8cdb5ba6eae5444757489796
shubhamsingh41 <shubham110904@gmail.com>
2025-03-26 15:13:55 +0530

    55 commit

---
+++ queuelinklist.java
@@ -0,0 +1,77 @@
+public class queuelinklist {
+     static class node{
+        int data;
+        node next;
+         
+        node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    static class que{
+        static node head = null;
+        static node tail = null;
+        
+       
+        public static boolean isEmpty(){
+            return head == null & tail == null;
+        }
+
+       
+        //enqueue
+        public static void add(int data){
+           node newnode = new node(data);
+           if(tail == null){
+            tail = head = newnode;
+            return;
+           }
+           tail.next = newnode;
+           tail = newnode;
+
+        }
+
+        //dequeue
+        public static int remove() {
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+
+           int front = head.data;
+           if(head == tail){
+            tail = null;
+           }
+           head = head.next;
+           return front;
+        }
+            //peek
+            public static int peek(){
+                if(isEmpty()){
+                    System.out.println("empty queue");
+                    return -1;
+                }
+                 return head.data;
+               
+            }     
+           }
+   
+    public static void main(String[] args) {
+        que q = new que();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        
+        
+        
+       while(!q.isEmpty()){
+        System.out.println(q.peek());
+        q.remove();
+       }
+        
+    }
+}
+
+
+
