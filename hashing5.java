2b343a683fc29c6fc19d5c5acb64cd58973d5398
shubhamsingh41 <shubham110904@gmail.com>
2025-04-02 21:07:18 +0530

    82 commit

---
+++ hashing5.java
@@ -0,0 +1,31 @@
+
+
+import java.util.HashMap;
+
+public class hashing5 {
+    //subarray sum equal to k
+    public static void main(String[] args) {
+        int arr[] = { 10,2,-2,-20,10};
+        int k = -10;
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+        int ans = 0;
+        int sum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+            if(map.containsKey(sum-k)){
+                ans += map.get(sum-k);
+            }
+            
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum) +1);
+
+            }else{
+                map.put(sum, 1);
+            }
+            
+        }
+        System.out.println(ans);
+    }
+}
